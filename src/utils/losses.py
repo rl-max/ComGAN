ae4d978b530a92c1f9c25727ea1194b887d1b650
rl-max <71051224+rl-max@users.noreply.github.com>
2022-12-07 11:37:59 +0900

    Update losses.py

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -246,7 +246,6 @@ def d_logistic_prob(d_logit_real, d_logit_fake, DDP, mixup_alpha = 1.0):
 def g_logistic_prob(d_logit_real, d_logit_fake, DDP, align_to_real=False):
     fake_label = 0.5 if align_to_real else 1.0
     prob = F.softplus(d_logit_fake) / (F.softplus(d_logit_fake) + F.softplus(d_logit_real))
-    print(prob)
     g_loss = fake_label * -torch.log(prob + 1e-10) + (1 - fake_label) * -torch.log(1 - prob + 1e-10)
     return g_loss.mean()
 
